2a8d3c133bd78078f9f827ffa4475b276e7c877b
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2024-08-25 15:09:36 -0300

    Primera parte de Element State css 1

---
+++ TA3_1.js
@@ -0,0 +1,5 @@
+const imagen = document.getElementById('imagen');
+
+imagen.addEventListener('mouseover', function () {
+    imagen.src = ''; //acá hay que poner el src de la imágen por la cual quieras cambiar la original
+});
